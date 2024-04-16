7252174a40e7269eab362c6baacc77e164ca2a62
root <jgc4c@mtmail.mtsu.edu>
2024-04-16 12:43:21 -0500

    Changed title of site to M.T.W. and to learn git commands

--- MTW.php
+++ MTW.php
@@ -3,12 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>M.T.W.</title>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jsonld/1.0.0/jsonld.min.js"></script>
     <script src="MTW.js"></script>
 </head>
-
 
 <body>
     <p id="test"></p>
