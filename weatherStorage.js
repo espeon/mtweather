afe74858fa6bbde83b25dd9b009d3e12d0acded6
RobertmillerMtsu <rlm7v@mtmail.mtsu.edu>
2024-04-16 12:20:28 -0500

    Added classes to be used in storing data after it is retrieved

---
+++ weatherStorage.js
@@ -0,0 +1,46 @@
+//use array size of 12
+class hourlyWeather{
+    constructor(number,startTime,endTime,isDaytime,
+                temperature,temperatureUnit,probabilityOfPrecipitation,
+                dewpoint,relativeHumidity,windSpeed,windDirection,
+                shortForcast,detailedForecast){
+
+
+                    this.number = number;
+                    this.startTime = startTime;
+                    this.endTime = endTime;
+                    this.isDaytime =isDaytime;
+                    this.temperature = temperature;
+                    this.temperatureUnit = temperatureUnit;
+                    this.probabilityOfPrecipitation= probabilityOfPrecipitation;
+                    this.dewpoint=dewpoint;
+                    this.relativeHumidity=relativeHumidity;
+                    this.windSpeed=windSpeed;
+                    this.windDirection=windDirection;
+                    this.shortForcast=shortForcast;
+                    this.detailedForecast=detailedForecast;
+    }
+}
+//use array size of 14
+class longPeriodWeather{
+    constructor(number,startTime,endTime,isDaytime,
+        temperature,temperatureUnit,probabilityOfPrecipitation,
+        dewpoint,relativeHumidity,windSpeed,windDirection,
+        shortForcast,detailedForecast){
+
+
+            this.number = number;
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.isDaytime =isDaytime;
+            this.temperature = temperature;
+            this.temperatureUnit = temperatureUnit;
+            this.probabilityOfPrecipitation= probabilityOfPrecipitation;
+            this.dewpoint=dewpoint;
+            this.relativeHumidity=relativeHumidity;
+            this.windSpeed=windSpeed;
+            this.windDirection=windDirection;
+            this.shortForcast=shortForcast;
+            this.detailedForecast=detailedForecast;
+    }
+}
